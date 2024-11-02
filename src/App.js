1dd29d9fb55c54334bb34808c4451fba08fd65c5
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2024-11-02 11:14:50 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
+import { useState } from 'react';
 import './App.css';
 import Aboutus from './component/aboutus';
 import CategoryList from './component/category';
 import Heder from './component/header';
 import Hero from './component/hero';
+import List from './component/list';
+import useAxios from './useAxios';
  
 function App() {
+  const [url, setUrl]= useState('/FastFood/list');
+  const [fastFoodItem , ,loading] = useAxios({
+    url
+  });
   return (
     <>
     <Heder/>
@@ -13,6 +20,7 @@ function App() {
       <Hero/>
       <Aboutus></Aboutus>
       <CategoryList></CategoryList>
+      <List fastFoodItem={fastFoodItem}></List>
       </main>
     </>
     
