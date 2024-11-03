6feafbcf06dce0fdb302b49b4c4330e0de572ea1
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2024-11-03 12:24:38 +0330

    Update list.jsx

--- src/component/list.jsx
+++ src/component/list.jsx
@@ -1,1 +1,38 @@
+import ItemList from "./listItem"
 
+const List = ({fastFoodItem})=>{
+
+    return(
+        <div className=" row " >
+
+
+            {
+                
+                fastFoodItem.map(List=>{
+                    return(
+
+                        <>
+                         <div className=" col-md-4 col-sm-6 tab-header text-center" key={List}>
+                         <div className="row gy-5">
+                            
+                                    <ItemList {...List}></ItemList>
+                            </div>
+                
+            </div>
+         
+                        </>
+                       
+                    )
+                })
+                
+
+            }
+            
+          
+           
+          </div>
+        
+    )
+}
+
+export default List
