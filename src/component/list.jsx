0f9a4581c4910fed8b98019537cf0033b29dacc4
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2024-11-06 09:26:48 +0330

    Update list.jsx

--- src/component/list.jsx
+++ src/component/list.jsx
@@ -1,6 +1,12 @@
+import { useThemecontext } from "../context"
 import ItemList from "./listItem"
 
+
+ 
 const List = ({fastFoodItem})=>{
+
+const themeContext=useThemecontext()
+const dark=themeContext.dark
 
     return(
         <div className=" row " >
@@ -12,10 +18,10 @@ const List = ({fastFoodItem})=>{
                     return(
 
                         <>
-                         <div className=" col-md-4 col-sm-6 tab-header text-center" key={List}>
-                         <div className="row gy-5">
+                         <div className= {`col-md-4 col-sm-6 tab-header ${dark ? 'bg-dark':'bg-light'} text-center`}>
+                         <div className="row gy-5" key={List.id}>
                             
-                                    <ItemList {...List}></ItemList>
+                                    <ItemList {...List} ></ItemList>
                             </div>
                 
             </div>
